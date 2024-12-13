e02aa7cbc5e5403c6825a14b770cb643c5fd61b5
Eirvairdre <pilotxplanes1@gmail.com>
2024-12-13 15:30:50 +1000

    Initial Commit

---
+++ Escape from Vim/Escape_from_VimApp.swift
@@ -0,0 +1,17 @@
+//
+//  Escape_from_VimApp.swift
+//  Escape from Vim
+//
+//  Created by Aleksandr Lysenko on 13.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Escape_from_VimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Escape from VimTests/Escape_from_VimTests.swift
@@ -0,0 +1,17 @@
+//
+//  Escape_from_VimTests.swift
+//  Escape from VimTests
+//
+//  Created by Aleksandr Lysenko on 13.12.2024.
+//
+
+import Testing
+@testable import Escape_from_Vim
+
+struct Escape_from_VimTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
